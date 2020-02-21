73a2243499db9adb76be8e4b5b3d5c26d3064f70
Paul Gass <paulgass@Pauls-MacBook-Pro.local>
2020-02-20 21:19:43 -0500

    bootstrap chart still testing

--- JS/BOOTSTRAP/Bootstrap.js
+++ JS/BOOTSTRAP/Bootstrap.js
@@ -1,17 +1,53 @@
-var x = function(idStr) {
-    return document.getElementById(idStr);
-};
+/* globals Chart:false, feather:false */
 
-var main = function() {
-    var num1 = x("inBox1").value;
-    var num2 = x("inBox2").value;
-    var answer = Math.sqrt(Math.pow(num1, 2) + Math.pow(num2, 2));
-
-    if (this.id == "btn1") {
-        x("outDiv").innerHTML = answer;
-    };
-};
-
-window.onload = function() {
-    x("btn1").addEventListener("click", main);
-};
+(function () {
+    'use strict'
+  
+    feather.replace()
+  
+    // Graphs
+    var ctx = document.getElementById('myChart')
+    // eslint-disable-next-line no-unused-vars
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: [
+          'Sunday',
+          'Monday',
+          'Tuesday',
+          'Wednesday',
+          'Thursday',
+          'Friday',
+          'Saturday'
+        ],
+        datasets: [{
+          data: [
+            15339,
+            21345,
+            18483,
+            24003,
+            23489,
+            24092,
+            12034
+          ],
+          lineTension: 0,
+          backgroundColor: 'transparent',
+          borderColor: '#007bff',
+          borderWidth: 4,
+          pointBackgroundColor: '#007bff'
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: false
+            }
+          }]
+        },
+        legend: {
+          display: false
+        }
+      }
+    })
+  })  
