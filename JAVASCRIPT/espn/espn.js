f75cd2f3e2143f7afc3365721cabd5f0bd6be46c
Gass, Paul CTR <paul.gass@isso.net>
2020-05-18 20:02:25 -0400

    espn 2

--- JAVASCRIPT/espn/espn.js
+++ JAVASCRIPT/espn/espn.js
@@ -1,1 +1,11 @@
-console.log("here is text from console.log() of espn.js script.")
+getScores = function() {
+  $.ajax({
+    type: "GET",
+    url: "http://espn.go.com/sports/scores/feed?sportId=0&eventId=0&rand=" + new Date().getTime() + "&xhr=1",
+    dataType: "json",
+    success: function (data) {
+      console.log(JSON.stringify(data, null, 2));
+    }
+  });
+}
+getScores();
