3d6d2fd8d01984226cb656ff2b61c4dc55097a70
Gass, Paul CTR <paul.gass@isso.net>
2020-05-25 12:12:34 -0400

    espn1

--- JAVASCRIPT/espn/espn.js
+++ JAVASCRIPT/espn/espn.js
@@ -1,11 +1,7 @@
-getScores = function() {
-  $.ajax({
-    type: "GET",
-    url: "http://espn.go.com/sports/scores/feed?sportId=0&eventId=0&rand=" + new Date().getTime() + "&xhr=1",
-    dataType: "json",
-    success: function (data) {
-      console.log(JSON.stringify(data, null, 2));
-    }
-  });
-}
-getScores();
+$.ajax({
+  url: "https://nataliegass.com/assets/json/nk.json",
+  dataType: 'json',
+  success: function(results){
+      console.log(results);
+  }
+});
