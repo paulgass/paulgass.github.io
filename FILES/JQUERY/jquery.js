546fddf021ba36baf0bb3350bcfdcce210249991
Paul Gass <paulm.gass@yahoo.com>
2021-01-25 13:01:21 -0500

    FILES

--- FILES/JQUERY/jquery.js
+++ FILES/JQUERY/jquery.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         var newHeading = "<h1>Hungry?</h1>";
         var newParagraph = document.createElement("p");
 		newParagraph.innerHTML = "<em>Bon appétit</em>";
-        var newImage = $('<img src="../_IMAGES/shrimp.png" alt="Symbol">');
+        var newImage = $('<img src="../IMAGES/shrimp.png" alt="Symbol">');
         $("#jQueryDivImageDisplay").append(newHeading, newParagraph, newImage);
     });
 });
